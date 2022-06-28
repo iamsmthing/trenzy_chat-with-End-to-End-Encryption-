0ed19936e6706af3f543a78e8670f03a28acbc13
SHIVAM KUMAR RAI <90586668+iamsmthing@users.noreply.github.com>
2022-06-28 14:54:48 +0530

    Add files via upload

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,13 @@
 const Messages = require("../models/messageModel");
+// crypto module
+const crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+// generate 16 bytes of random data
+const initVector = crypto.randomBytes(16);
+
+// secret key generate 32 bytes of random data
+const Securitykey = crypto.randomBytes(32);
 
 module.exports.getMessages = async (req, res, next) => {
   try {
@@ -11,9 +20,19 @@ module.exports.getMessages = async (req, res, next) => {
     }).sort({ updatedAt: 1 });
 
     const projectedMessages = messages.map((msg) => {
+      const decipher = crypto.createDecipheriv(
+        algorithm,
+        Securitykey,
+        initVector
+      );
+
+      let decryptedData = decipher.update(smg.message.text, "hex", "utf-8");
+
+      decryptedData += decipher.final("utf8");
+
       return {
         fromSelf: msg.sender.toString() === from,
-        message: msg.message.text,
+        message: decryptedData,
       };
     });
     res.json(projectedMessages);
@@ -24,9 +43,17 @@ module.exports.getMessages = async (req, res, next) => {
 
 module.exports.addMessage = async (req, res, next) => {
   try {
+    //encryption
+
+    // the cipher function
+    const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+
     const { from, to, message } = req.body;
+    let encryptedData = cipher.update(message, "utf-8", "hex");
+
+    encryptedData += cipher.final("hex");
     const data = await Messages.create({
-      message: { text: message },
+      message: { text: encryptedData },
       users: [from, to],
       sender: from,
     });
